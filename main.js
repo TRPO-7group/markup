35bf36ec30782bbb496b42d3f07f842d4154eacc
aleksey kotelevskiy <a_kotelevskiy@mail.ru>
2017-11-04 17:38:05 +0300

    Деталька личного репа

--- main.js
+++ main.js
@@ -21,3 +21,15 @@ $(document).on("click", ".list-group > span", function () {
     else
         $(this).parent().find(".list-elem").show(200);
 })
+
+
+$(document).on("click", ".detail-statistic", function () {
+    if ($(".detail-commits-all").is(":visible"))
+    {
+        $(this).find("span").html("[+]");
+        $(".detail-commits-all").hide(200);
+    } else {
+        $(this).find("span").html("[-]");
+        $(".detail-commits-all").show(200);
+    }
+})
